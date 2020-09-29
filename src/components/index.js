782ba796bde2a576d1324fc1bb01c8406b81b70d
Vue_Component <48897146+yannanna0920@users.noreply.github.com>
2020-09-29 15:03:35 +0800

    Add files via upload

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,67 @@
-export { default as CheckboxItem } from './checkbox'
-export { default as ButtonItem } from './button'
-export { default as HomeTitle } from './home-title'
-export { default as InputItem } from './input-item'
-export { default as InputNumber } from './input-number'
-export { default as ItemList } from './item-list'
-export { default as Loading } from './loading'
-export { default as Popup } from './popup'
-export { default as Tag } from './tag'
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text, Image } from '@tarojs/components'
+import { AtTabs, AtTabsPane, AtButton } from 'taro-ui'
+import './index.scss'
+
+export default class AtTab extends Component {
+  //   static propTypes = {
+  //     tabChange: PropTypes.func,
+  // }
+  static defaultProps = {
+    tabDirection: 'horizontal', //horizontal'，'vertical'
+    height: '',
+    scroll: false,
+    swipeable: true, //是否支持手势滑动切换内容页
+    animated: true,
+    tabList: []
+  }
+  constructor() {
+    super(...arguments)
+    this.state = {
+      current: 0
+    }
+  }
+  handleClick(value) {
+    this.props.tabChange(value)
+    this.setState({
+      current: value
+    })
+  }
+  render() {
+    const {
+      tabList,
+      tabDirection,
+      height,
+      swipeable,
+      scroll,
+      animated,
+      content
+    } = this.props
+    const { current } = this.state
+    return (
+      <AtTabs
+        tabDirection={tabDirection}
+        height={height}
+        swipeable={swipeable}
+        scroll={scroll}
+        animated={animated}
+        current={current}
+        tabList={tabList}
+        onClick={this.handleClick.bind(this)}
+      > 
+         {tabList.map((tab, i) => {
+            return (
+              <AtTabsPane
+                current={current}
+                index={current}
+                key={i}
+              >
+                {content}
+               
+              </AtTabsPane>
+            )
+          })}
+      </AtTabs>
+    )
+  }
+}
